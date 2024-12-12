af9865afbb1bde0d0c2e1e48c81767f1e2cb82ce
Lahiru Amarasooriya <lahirudx@gmail.com>
2024-12-13 00:19:47 +0530

    Reanimated and gesture handler setup

--- App.tsx
+++ App.tsx
@@ -2,21 +2,19 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Game from "@/components/Game";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <>
+    <GestureHandlerRootView style={styles.container}>
       <Game />
       <StatusBar style="auto" />
-    </>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
--- components/Game.tsx
+++ components/Game.tsx
@@ -1,10 +1,10 @@
 import { boardHeight } from "@/constants";
-import { SafeAreaView, View, StyleSheet } from "react-native";
+import { SafeAreaView, View, StyleSheet, Text } from "react-native";
 
 export default function Game() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.board}>{/* TODO: Add game elements */}</View>
+      <View style={styles.board}></View>
     </SafeAreaView>
   );
 }
